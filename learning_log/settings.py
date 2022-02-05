84e99492e28f0a563f251fb6fbea74f1592c6074
Maciej Olszański <olszanskimaciej@wp.pl>
2022-02-05 10:49:08 +0100

    change style of sites: index, topic, topics, login

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -35,6 +35,9 @@ INSTALLED_APPS = [
     'learning_logs',
     'users',
 
+    # foreign apps
+    'bootstrap4',
+
     # django default apps
     'django.contrib.admin',
     'django.contrib.auth',
